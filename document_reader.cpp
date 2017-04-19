07c467bef3f6d4c5316a59ae988e41f63c42e10d
Ivan Beresnev <i.beresnev@osnovalab.ru>
2017-04-19 19:03:26 +0300

    Add check document is regular file

--- document_reader.cpp
+++ document_reader.cpp
@@ -1,25 +1,37 @@
 #include "document_reader.h"
 #include <fstream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+//------------------------------------------------------------------------------
+int isRegularFile(const char *aPath)
+{
+  struct stat path_stat;
+  stat(aPath, &path_stat);
+  return S_ISREG(path_stat.st_mode);
+}
 //------------------------------------------------------------------------------
 bool DocumentReader::read(std::string const &aDocument, std::ostream &aStream) {
   bool success = false;
 
-  std::ifstream ifl(aDocument, std::ios_base::in | std::ios_base::binary);
-  if (ifl) {
-    ifl.seekg(0, std::ios_base::end);
-    auto const seekPos = ifl.tellg();
-    ifl.seekg(0, std::ios_base::beg);
+  if ( isRegularFile( aDocument.c_str() ) ) {
+    std::ifstream ifl(aDocument, std::ios_base::in | std::ios_base::binary);
+    if (ifl) {
+      ifl.seekg(0, std::ios_base::end);
+      auto const seekPos = ifl.tellg();
+      ifl.seekg(0, std::ios_base::beg);
 
-    int const bufSize = seekPos - ifl.tellg();
-    if (bufSize >= 0) {
-      success = true;
+      int const bufSize = seekPos - ifl.tellg();
+      if (bufSize >= 0) {
+        success = true;
 
-      auto buf = new char[bufSize];
-      ifl.read(buf, bufSize);
+        auto buf = new char[bufSize];
+        ifl.read(buf, bufSize);
 
-      aStream.write(buf, bufSize);
+        aStream.write(buf, bufSize);
 
-      delete buf;
+        delete buf;
+      }
     }
   }
 
